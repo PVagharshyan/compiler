7a0ad7fd1d65b2d5e380492158583b7c6baaf490
PVagharshyan <paylak.vagharshyan@gmail.com>
2026-04-19 15:22:18 +0400

    Fix(style): lexer.cpp

--- lexer/src/lexer.cpp
+++ lexer/src/lexer.cpp
@@ -23,46 +23,46 @@ bool lexer::isKeyword(const std::string& w) const {
 }
 
 Token lexer::nextToken() {
-        skipSpaces();
-        char c = peek();
+    skipSpaces();
+    char c = peek();
 
-        if (c == '\0')
-            return {TokenType::EndOfFile, ""};
+    if (c == '\0')
+        return {TokenType::EndOfFile, ""};
 
-        // IDENT / KEYWORD
-        if (isalpha(c) || c == '_') {
-            std::string w;
-            while (isalnum(peek()) || peek() == '_')
-                w += get();
+    // IDENT / KEYWORD
+    if (isalpha(c) || c == '_') {
+        std::string w;
+        while (isalnum(peek()) || peek() == '_')
+            w += get();
 
-            if (w == "if") return {TokenType::If, w};
-            if (w == "else") return {TokenType::Else, w};
-            if (w == "while") return {TokenType::While, w};
-            if (w == "for") return {TokenType::For, w};
+        if (w == "if") return {TokenType::If, w};
+        if (w == "else") return {TokenType::Else, w};
+        if (w == "while") return {TokenType::While, w};
+        if (w == "for") return {TokenType::For, w};
 
-            return {TokenType::Identifier, w};
-        }
-
-        // NUMBER
-        if (isdigit(c)) {
-            std::string n;
-            while (isdigit(peek()))
-                n += get();
-
-            return {TokenType::Number, n};
-        }
-
-        // SYMBOLS
-        switch (get()) {
-            case '{': return {TokenType::LBrace, "{"};
-            case '}': return {TokenType::RBrace, "}"};
-            case '(': return {TokenType::LParen, "("};
-            case ')': return {TokenType::RParen, ")"};
-            case ';': return {TokenType::Semicolon, ";"};
-            case '=': return {TokenType::Assign, "="};
-            case '+': case '-': case '*': case '/':
-                return {TokenType::Operator, std::string(1, c)};
-        }
-
-        return {TokenType::Unknown, std::string(1, get())};
+        return {TokenType::Identifier, w};
     }
+
+    // NUMBER
+    if (isdigit(c)) {
+        std::string n;
+        while (isdigit(peek()))
+            n += get();
+
+        return {TokenType::Number, n};
+    }
+
+    // SYMBOLS
+    switch (get()) {
+        case '{': return {TokenType::LBrace, "{"};
+        case '}': return {TokenType::RBrace, "}"};
+        case '(': return {TokenType::LParen, "("};
+        case ')': return {TokenType::RParen, ")"};
+        case ';': return {TokenType::Semicolon, ";"};
+        case '=': return {TokenType::Assign, "="};
+        case '+': case '-': case '*': case '/':
+            return {TokenType::Operator, std::string(1, c)};
+    }
+
+    return {TokenType::Unknown, std::string(1, get())};
+}
